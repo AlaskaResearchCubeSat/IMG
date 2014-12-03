884d593f584079475833bc78f5691a105654cc91
Jesse Frey <jmfrey@alaska.edu>
2014-12-03 12:13:30 -0900

    Added a gap between error data and image data

--- IMG.h
+++ IMG.h
@@ -14,7 +14,7 @@
     //define the size of a image slot
     #define IMG_SLOT_SIZE           (100)
     //define address ranges for image data
-    enum{IMG_ADDR_START=ERR_ADDR_END+1,IMG_ADDR_END=IMG_ADDR_START+NUM_IMG_SLOTS*IMG_SLOT_SIZE};
+    enum{IMG_ADDR_START=ERR_ADDR_END+10,IMG_ADDR_END=IMG_ADDR_START+NUM_IMG_SLOTS*IMG_SLOT_SIZE};
 
     //read and write slots for picture
     extern int readPic,writePic;
